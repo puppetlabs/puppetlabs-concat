0bbbe320ccd6df0b8e51b9b9dfc9debdf1e2654a
Gildas Le Nadan <mael@tirnamban.org>
2012-05-20 20:24:22 +0200

    add information about concat_basedir and the need for pluginsync = true at least once

--- manifests/setup.pp
+++ manifests/setup.pp
@@ -1,8 +1,8 @@
 # Sets up the concat system.
 #
-# $concatdir should point to a place where you wish the fragments to
-# live. This should not be somewhere like /tmp since ideally these files
-# should not be deleted ever, puppet should always manage them
+# $concatdir is where the fragments live and is set on the fact concat_basedir.
+# Since puppet should always manage files in $concatdir and they should
+# not be deleted ever, /tmp is not an option.
 #
 # $puppetversion should be either 24 or 25 to enable a 24 compatible
 # mode, in 24 mode you might see phantom notifies this is a side effect
