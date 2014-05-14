33b90b4fbc165061722477bce956cf0eeb1f9761
Morgan Haskel <morgan@puppetlabs.com>
2014-05-14 16:10:03 -0400

    Fixes to get tests running on trusty.

--- spec/spec_helper_acceptance.rb
+++ spec/spec_helper_acceptance.rb
@@ -1,19 +1,17 @@
 require 'beaker-rspec/spec_helper'
 require 'beaker-rspec/helpers/serverspec'
 
-unless ENV['RS_PROVISION'] == 'no'
+unless ENV['RS_PROVISION'] == 'no' or ENV['BEAKER_provision'] == 'no'
+  if hosts.first.is_pe?
+    install_pe
+  else
+    install_puppet
+  end
   hosts.each do |host|
     if host['platform'] =~ /debian/
       on host, 'echo \'export PATH=/var/lib/gems/1.8/bin/:${PATH}\' >> ~/.bashrc'
     end
-    if host.is_pe?
-      install_pe
-    else
-      # Install Puppet
-      install_package host, 'rubygems'
-      on host, 'gem install puppet --no-ri --no-rdoc'
-      on host, "mkdir -p #{host['distmoduledir']}"
-    end
+    on hosts, "mkdir -p #{host['distmoduledir']}"
   end
 end
 
