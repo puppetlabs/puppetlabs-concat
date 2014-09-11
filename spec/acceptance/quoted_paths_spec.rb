344f6adb36910f3f6cb447669d7b32aed8a624a2
Morgan Haskel <morgan@puppetlabs.com>
2014-09-11 12:18:05 -0400

    Try to remove all the platform dependent stuff
    And updated the tests to work as planned

--- spec/acceptance/quoted_paths_spec.rb
+++ spec/acceptance/quoted_paths_spec.rb
@@ -1,21 +1,30 @@
 require 'spec_helper_acceptance'
 
 describe 'quoted paths' do
+  basedir = default.tmpdir('concat')
+
   before(:all) do
-    shell('rm -rf "/tmp/concat test" /var/lib/puppet/concat')
-    shell('mkdir -p "/tmp/concat test"')
+    pp = <<-EOS
+      file { '#{basedir}':
+        ensure => directory,
+      }
+      file { '#{basedir}/concat test':
+        ensure => directory,
+      }
+    EOS
+    apply_manifest(pp)
   end
 
   context 'path with blanks' do
     pp = <<-EOS
-      concat { '/tmp/concat test/foo':
+      concat { '#{basedir}/concat test/foo':
       }
       concat::fragment { '1':
-        target  => '/tmp/concat test/foo',
+        target  => '#{basedir}/concat test/foo',
         content => 'string1',
       }
       concat::fragment { '2':
-        target  => '/tmp/concat test/foo',
+        target  => '#{basedir}/concat test/foo',
         content => 'string2',
       }
     EOS
@@ -25,7 +34,7 @@ describe 'quoted paths' do
       apply_manifest(pp, :catch_changes => true)
     end
 
-    describe file('/tmp/concat test/foo') do
+    describe file("#{basedir}/concat test/foo") do
       it { should be_file }
       it { should contain "string1\nsring2" }
     end
