e4021900e19c4edc779634244ed817f4a9e00672
Eimhin Laverty <eimhin.laverty@puppet.com>
2019-02-15 13:27:44 +0000

    Test refactor for acceptance/noop_spec

--- spec/acceptance/noop_spec.rb
+++ spec/acceptance/noop_spec.rb
@@ -1,35 +1,26 @@
 require 'spec_helper_acceptance'
 
-describe 'concat noop parameter', unless: (fact('kernel') != 'Linux') do
-  basedir = default.tmpdir('concat')
-  context 'with "/usr/bin/test -e %"' do
-    before(:all) do
-      pp = <<-MANIFEST
-        file { '#{basedir}':
-          ensure => directory
-        }
-      MANIFEST
-
-      apply_manifest(pp)
-    end
-    pp = <<-MANIFEST
-      concat_file { '#{basedir}/file':
+describe 'concat noop parameter', if: ['debian', 'redhat', 'ubuntu'].include?(os[:family]) do
+  before(:all) do
+    @basedir = setup_test_directory
+  end
+  describe 'with "/usr/bin/test -e %"' do
+    let(:pp) do
+      <<-MANIFEST
+      concat_file { '#{@basedir}/file':
         noop => false,
       }
       concat_fragment { 'content':
-        target  => '#{basedir}/file',
+        target  => '#{@basedir}/file',
         content => 'content',
       }
     MANIFEST
-
-    it 'applies the manifest twice with no stderr' do
-      apply_manifest(pp, expect_changes: true, noop: true)
-      apply_manifest(pp, catch_changes: true, noop: true)
     end
 
-    describe file("#{basedir}/file") do
-      it { is_expected.to be_file }
-      it { is_expected.to contain 'content' }
+    it 'applies the manifest twice with no stderr' do
+      idempotent_apply(default, pp)
+      expect(file("#{@basedir}/file")).to be_file
+      expect(file("#{@basedir}/file").content).to contain 'content'
     end
   end
 end
