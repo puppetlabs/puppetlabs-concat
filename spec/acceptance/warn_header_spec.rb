0bbf3e0522561cf36677d9497479ec7a63c8314e
Eimhin Laverty <eimhin.laverty@puppet.com>
2019-02-15 13:47:28 +0000

    Test refactor for acceptance/warn_header_spec

--- spec/acceptance/warn_header_spec.rb
+++ spec/acceptance/warn_header_spec.rb
@@ -1,116 +1,97 @@
 require 'spec_helper_acceptance'
 
 describe 'concat warn =>' do
-  basedir = default.tmpdir('concat')
-  context 'when true should enable default warning message' do
-    pp = <<-MANIFEST
-      concat { '#{basedir}/file':
+  before(:all) do
+    @basedir = setup_test_directory
+  end
+
+  describe 'when true should enable default warning message' do
+    let(:pp) do
+      <<-MANIFEST
+      concat { '#{@basedir}/file':
         warn  => true,
       }
 
       concat::fragment { '1':
-        target  => '#{basedir}/file',
+        target  => '#{@basedir}/file',
         content => '1',
         order   => '01',
       }
 
       concat::fragment { '2':
-        target  => '#{basedir}/file',
+        target  => '#{@basedir}/file',
         content => '2',
         order   => '02',
       }
     MANIFEST
+    end
 
     it 'applies the manifest twice with no stderr' do
-      apply_manifest(pp, catch_failures: true)
-      apply_manifest(pp, catch_changes: true)
-    end
-
-    describe file("#{basedir}/file") do
-      it { is_expected.to be_file }
-      its(:content) do
-        is_expected.to match %r{# This file is managed by Puppet\. DO NOT EDIT\.}
-      end
-      its(:content) do
-        is_expected.to match %r{1}
-      end
-      its(:content) do
-        is_expected.to match %r{2}
-      end
+      idempotent_apply(default, pp)
+      expect(file("#{@basedir}/file")).to be_file
+      expect(file("#{@basedir}/file").content).to match %r{# This file is managed by Puppet\. DO NOT EDIT\.}
+      expect(file("#{@basedir}/file").content).to match %r{1}
+      expect(file("#{@basedir}/file").content).to match %r{2}
     end
   end
-  context 'when false should not enable default warning message' do
-    pp = <<-MANIFEST
-      concat { '#{basedir}/file':
+
+  describe 'when false should not enable default warning message' do
+    let(:pp) do
+      <<-MANIFEST
+      concat { '#{@basedir}/file':
         warn  => false,
       }
 
       concat::fragment { '1':
-        target  => '#{basedir}/file',
+        target  => '#{@basedir}/file',
         content => '1',
         order   => '01',
       }
 
       concat::fragment { '2':
-        target  => '#{basedir}/file',
+        target  => '#{@basedir}/file',
         content => '2',
         order   => '02',
       }
     MANIFEST
+    end
 
     it 'applies the manifest twice with no stderr' do
-      apply_manifest(pp, catch_failures: true)
-      apply_manifest(pp, catch_changes: true)
-    end
-
-    describe file("#{basedir}/file") do
-      it { is_expected.to be_file }
-      its(:content) do
-        is_expected.not_to match %r{# This file is managed by Puppet\. DO NOT EDIT\.}
-      end
-      its(:content) do
-        is_expected.to match %r{1}
-      end
-      its(:content) do
-        is_expected.to match %r{2}
-      end
+      idempotent_apply(default, pp)
+      expect(file("#{@basedir}/file")).to be_file
+      expect(file("#{@basedir}/file").content).not_to match %r{# This file is managed by Puppet\. DO NOT EDIT\.}
+      expect(file("#{@basedir}/file").content).to match %r{1}
+      expect(file("#{@basedir}/file").content).to match %r{2}
     end
   end
-  context 'when foo should overide default warning message' do
-    pp = <<-MANIFEST
-      concat { '#{basedir}/file':
+
+  describe 'when foo should overide default warning message' do
+    let(:pp) do
+      <<-MANIFEST
+      concat { '#{@basedir}/file':
         warn  => "# foo\n",
       }
 
       concat::fragment { '1':
-        target  => '#{basedir}/file',
+        target  => '#{@basedir}/file',
         content => '1',
         order   => '01',
       }
 
       concat::fragment { '2':
-        target  => '#{basedir}/file',
+        target  => '#{@basedir}/file',
         content => '2',
         order   => '02',
       }
     MANIFEST
-
-    it 'applies the manifest twice with no stderr' do
-      apply_manifest(pp, catch_failures: true)
-      apply_manifest(pp, catch_changes: true)
     end
 
-    describe file("#{basedir}/file") do
-      it { is_expected.to be_file }
-      its(:content) do
-        is_expected.to match %r{# foo}
-      end
-      its(:content) do
-        is_expected.to match %r{1}
-      end
-      its(:content) do
-        is_expected.to match %r{2}
-      end
+    it 'applies the manifest twice with no stderr' do
+      idempotent_apply(default, pp)
+      expect(file("#{@basedir}/file")).to be_file
+      expect(file("#{@basedir}/file").content).to match %r{# foo}
+      expect(file("#{@basedir}/file").content).to match %r{1}
+      expect(file("#{@basedir}/file").content).to match %r{2}
     end
   end
 end
