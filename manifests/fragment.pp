909606b8ef994db00bddb7cbef32b0c358e88f32
Lightning <lightning@lightning-online.de>
2023-07-18 17:41:11 +0200

    Add parameter "tag" to concat::fragment
    allow a user defined tag for a fragment; keep previous default behavior
    if not supplied

--- manifests/fragment.pp
+++ manifests/fragment.pp
@@ -16,10 +16,14 @@
 # @param target
 #   Specifies the destination file of the fragment. Valid options: a string containing the path or title of the parent concat resource.
 #
+# @param tag
+#   Specifies a custom tag to use for the fragment.
+#
 define concat::fragment (
   String                                                 $target,
   Optional[Variant[Sensitive[String], String, Deferred]] $content = undef,
   Optional[Variant[String, Array]]                       $source  = undef,
+  Optional[String[1]]                                    $tag     = undef,
   Variant[String, Integer]                               $order   = '10',
 ) {
   $resource = 'Concat::Fragment'
@@ -34,7 +38,11 @@ define concat::fragment (
     fail("${resource}['${title}']: Can't use 'source' and 'content' at the same time.")
   }
 
-  $safe_target_name = regsubst($target, '[\\\\/:~\n\s\+\*\(\)@]', '_', 'GM')
+  if $tag =~ Undef {
+    $safe_target_name = regsubst($target, '[\\\\/:~\n\s\+\*\(\)@]', '_', 'GM')
+  } else {
+    $safe_target_name = $tag
+  }
 
   concat_fragment { $name:
     target  => $target,
