ec66d1f78893a8e4e47165dce0eb76a1519cbe3d
Jason St-Cyr <jst-cyr@users.noreply.github.com>
2026-09-21 15:49:34 -0400

    Adding test case to test against multi-line content

--- spec/acceptance/deduplicate_fragments_spec.rb
+++ spec/acceptance/deduplicate_fragments_spec.rb
@@ -63,4 +63,35 @@ describe 'concat deduplicate_fragments' do
       expect(file("#{basedir}/foo").content).to match(%r{\Aduplicateunique\r?\n?\z})
     end
   end
+
+  describe 'when deduplicate_fragments is true with multi-line fragment content' do
+    let(:pp) do
+      <<-MANIFEST
+        concat { '#{basedir}/multiline':
+          deduplicate_fragments => true,
+        }
+        concat::fragment { '1':
+          target  => '#{basedir}/multiline',
+          content => "line1\nline2\n",
+          order   => '20',
+        }
+        concat::fragment { '2':
+          target  => '#{basedir}/multiline',
+          content => "line1\nline2\n",
+          order   => '10',
+        }
+        concat::fragment { '3':
+          target  => '#{basedir}/multiline',
+          content => "line3\n",
+          order   => '30',
+        }
+      MANIFEST
+    end
+
+    it 'idempotent, deduplicates multi-line fragments regardless of platform line endings' do
+      idempotent_apply(pp)
+      expect(file("#{basedir}/multiline")).to be_file
+      expect(file("#{basedir}/multiline").content).to match(%r{\Aline1\r?\nline2\r?\nline3\r?\n?\z})
+    end
+  end
 end
